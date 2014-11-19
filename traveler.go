11164b29190d7b5dbfdbf6309a91de870b890591
Alfonso Kim <alfonsokim84@gmail.com>
2014-11-18 23:10:33 -0600

    avances, falta revisar la permutacion

--- traveler.go
+++ traveler.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/fighterlyt/permutation"
+	"math"
 )
 
 type City struct {
@@ -10,9 +12,17 @@ type City struct {
 	lat  int
 	lon  int
 }
+type Path struct {
+	cities   []*City
+	distance float64
+}
 
 func NewCity(n string, x int, y int) *City {
 	return &City{name: n, lat: x, lon: y}
+}
+
+func NewPath(c []*City, d float64) *Path {
+	return &Path{cities: c, distance: d}
 }
 
 func ReadCities(path string) ([]*City, error) {
@@ -23,11 +33,29 @@ func ReadCities(path string) ([]*City, error) {
 }
 
 func lessCity(i, j interface{}) bool {
-	return true
+	return true // no importa el orden en el que devuelve las permutaciones
 }
 
 func (c City) String() string {
 	return fmt.Sprintf("%s: %d,%d", c.name, c.lat, c.lon)
+}
+
+func (p Path) String() string {
+	var buffer bytes.Buffer
+	for i := 0; i < len(p.cities)-1; i++ {
+		buffer.WriteString(p.cities[i].name)
+		buffer.WriteString(" -> ")
+	}
+	buffer.WriteString(p.cities[len(p.cities)-1].name)
+	buffer.WriteString(": ")
+	buffer.WriteString(fmt.Sprintf("%.2f", p.distance))
+	return buffer.String()
+}
+
+func (c City) Distance(other City) float64 {
+	x := float64(c.lat - other.lat)
+	y := float64(c.lon - other.lon)
+	return math.Sqrt(x*x + y*y)
 }
 
 func main() {
@@ -37,12 +65,22 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	paths := make([]Path, p.Left())
 	for c, err := p.Next(); err == nil; c, err = p.Next() {
-		//fmt.Printf("%3d permutation: %v left %d\n", p.Index()-1, i.([]int), p.Left())
 		citiesPermutation := c.([]*City)
-		for _, perm := range citiesPermutation {
-			fmt.Println(perm)
+		totalDistance := float64(0)
+		for i := 0; i < len(citiesPermutation)-1; i++ {
+			from := citiesPermutation[i]
+			to := citiesPermutation[i+1]
+			distance := from.Distance(*to)
+			//fmt.Println("de ", from, " a ", to, " = ", distance)
+			totalDistance += distance
 		}
-		fmt.Printf("permutation: %d left %d\n", p.Index()-1, p.Left())
+		paths[p.Index()-1] = *NewPath(citiesPermutation, totalDistance)
+		//fmt.Printf("permutation: %d left %d\n", p.Index()-1, p.Left())
+	}
+	for _, path := range paths {
+		fmt.Println("Path: ", path)
 	}
 }
